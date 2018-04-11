397f4b29edcc08901307bd82df38112d9102490e
streamedLine <dovidlwtn@gmail.com>
2018-04-10 21:16:57 -0400

    setup serializer to provide correct info

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
 
   # This method gives us a simple call to check if a user has permission to modify.
   def can_modify_user?(user_id)
-    role == 'admin' || id.to_s == user_id.to_s
+    is_admin? || id.to_s == user_id.to_s
   end
 
   # This method tells us if the user is an admin or not.
