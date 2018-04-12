7f5fdfc72f2fa4b13ce89bc5d86b3b3efc186b1c
streamedLine <dovidlwtn@gmail.com>
2018-04-11 22:57:17 -0400

    import react-router-dom create login- link, form and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
+//import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 //import { fetchToken } from './actions/userActions'
 import logo from './logo.svg';
 import './App.css';
+
+const loginForm = () => {
+  return (
+    <form onSubmit>
+      Email <input type="text" value="email" name="email" />
+      <br/>
+      Password <input type="password" />
+      <br/>
+      <input type="submit" value="Login" />
+    </form>
+  )
+}
 
 class App extends Component {
 
@@ -15,28 +28,31 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {this.props.token}
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to the Future</h1>
+            <Link to="/login">Login</Link>
+          </header>
+        
+
+          <Route path="/login" render={loginForm} />
+        </div>
+      </Router>  
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {token: state.token}
+}
+
+export default connect(mapStateToProps)(App);
 
 //DEV ONLY BELOW
-// const mapStateToProps = (state) => {
-//   return {token: state.token}
-// }
-
 // const mapDispatchToProps = (dispatch) => {
 //   return bindActionCreators({fetchToken: fetchToken}, dispatch)
 // }
 
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,13 +1,14 @@
-const initialState = {token: false};
+const initialState = {token: false, loggedIn: false};
 
 export default (state = initialState, action) => {
 	console.log(state, action.type)
 	switch (action.type) {
 		case 'FETCH_TOKEN':
-			return {token: action.payload}
+		  const newState = action.payload ? {token: action.payload, loggedIn: true} : {token: false, loggedIn: false};
+			return newState
 
 		case 'AUTHENTICATE_USER': 
-			return action.authenticated ? state : {token: false}
+			return action.authenticated ? state : {token: false, loggedIn: false};
 
 		default:
 			return state
