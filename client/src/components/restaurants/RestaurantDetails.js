15edeb03491817cfa48a0800a0afb61baca76d37
streamedLine <dovidlwtn@gmail.com>
2018-04-15 17:24:03 -0400

    add googlemap with marker to restaurant view pages

--- client/src/components/restaurants/RestaurantDetails.js
+++ client/src/components/restaurants/RestaurantDetails.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import key from '../../private/google-key'
 
 export default class RestaurantDetails extends React.Component {
 	render() {
 		const restaurant = this.props.restaurant;
-
+		console.log(key)
 		return (
 			<div className='restaurantDetails'>
 				{restaurant &&
@@ -14,6 +15,14 @@ export default class RestaurantDetails extends React.Component {
 						<p>{restaurant.address} {restaurant.zip}</p>
 						<h4>Visit our website</h4>
 						<p>{restaurant.website}</p>
+						<iframe
+						  width="600"
+						  height="450"
+						  frameBorder="0" style={{border: '4px solid grey'}}
+						  src={`https://www.google.com/maps/embed/v1/place?key=${key}
+						  						    &q=${encodeURI(restaurant.address+' '+restaurant.zip)}`} allowFullScreen>
+						  }
+						</iframe>
 					</div>										}
 			</div>
 		)
