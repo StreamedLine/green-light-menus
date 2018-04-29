07a6575331cae135a3cd79c3767abed4fb61c080
streamedLine <dovidlwtn@gmail.com>
2018-04-29 10:39:26 -0400

    fix bug use defaultChecked instead if checked

--- client/src/components/allergies/AllergyCheckboxes.js
+++ client/src/components/allergies/AllergyCheckboxes.js
@@ -5,11 +5,14 @@ class AllergyCheckboxes extends React.Component {
 	render() {
 		const menu_id = this.props.menu_id || 'default';
 		const boxes = this.props.allergies.map((allergy, i) => {
-			const checked = this.props.menuItem.allergies.find( a => a.name === allergy.name) ? 'checked' : null;
+			
+			if (this.props.menuItem) {
+				var checked = this.props.menuItem.allergies.find( a => a.name === allergy.name) ? 'checked' : null;
+			}
 
 			return (
 				<div key={i}>
-					<input type="checkbox" id={`${allergy.name}-${this.props.menu_id}`} name={`${allergy.name}-${this.props.menu_id}`} checked={checked}/> 
+					<input type="checkbox" id={`${allergy.name}-${this.props.menu_id}`} name={`${allergy.name}-${this.props.menu_id}`} defaultChecked={checked} /> 
 					<label htmlFor={`${allergy.name}-${this.props.menu_id}`}>{allergy.name}</label>
 				</div>
 			)
